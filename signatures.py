8c815f08dd980364473c48385ca2bd8eb5fd9159
IwinosaOsayande <winnersosayande2005@gmail.com>
2026-06-14 14:29:28 +0200

    Signatures code

---
+++ signatures.py
@@ -0,0 +1,8 @@
+SIGNATURES = {
+    bytes.fromhex("89504E470D0A1A0A"): "PNG",
+    bytes.fromhex("FFD8FF"): "JPEG",
+    bytes.fromhex("25504446"): "PDF",
+    bytes.fromhex("504B0304"): "ZIP",
+    bytes.fromhex("4D5A"): "EXE",
+    bytes.fromhex("7F454C46"): "ELF"
+}
