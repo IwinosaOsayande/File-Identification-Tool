c7e7bd808bfecb8a4c7798b3d660fde9f72f8cc3
IwinosaOsayande <winnersosayande2005@gmail.com>
2026-06-14 14:27:02 +0200

    Report code
    Generate a report of scanned files and their types.

--- report.py
+++ report.py
@@ -1,1 +1,18 @@
+from scanner import scan_directory
 
+folder = input("Folder to scan: ")
+
+results = scan_directory(folder)
+
+with open("report.txt", "w") as report:
+
+    report.write("FILE INSPECTOR REPORT\n")
+    report.write("=" * 50 + "\n\n")
+
+    for item in results:
+
+        report.write(
+            f"{item['file']} -> {item['type']}\n"
+        )
+
+print("Report saved as report.txt")
